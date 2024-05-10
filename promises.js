1399da7423f645a4c102801308023df530bf28ce
Agnes-Kamondi <kamondiagnesauma@gmail.com>
2024-05-10 17:25:56 +0300

    made changes

--- promises.js
+++ promises.js
@@ -1,13 +1,16 @@
 //Write an asynchronous function that accepts a message string and a delay time in milliseconds.
 // The function should log the message to the console after the specified delay time.
 
-const promise = new Promise(function(resolve) {
-    setTimeout(function() {
-     const greeting = "My name is Agnes Auma";
-     resolve(greeting);
-     console.log({greeting})
-    }, 5000);
-   });
+let promise = new Promise(function (resolve){
+    setTimeout(function(){
+        resolve ("Wake up everyday and show up")}, 5000);
+   })
+async function logMessageAfterDelay(message,delayTime){
+        let result = await promise;
+        console.log({result});
+        console.log("Hello, time has been delayed by four seconds");
+    }
+logMessageAfterDelay();
 
    //You have an array of user IDs and a function getUserData(id) that returns a Promise with user data when given
    // a user ID. Write an asynchronous function that fetches and logs the data for each user ID one by one, in sequence.
